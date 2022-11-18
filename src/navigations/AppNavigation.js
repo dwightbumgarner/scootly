2f263f1bd1fdc9b4edb09400ff500a8d1323b98e
Dwight Bumgarner <dwightbumgarner@gmail.com>
2022-11-17 16:06:20 -0800

    Adding vendor flow

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -69,7 +69,6 @@ const HomeStack = () => (
       component={ProfileScreen}
       style={styles.homeHeader}
     />
-    <Stack.Screen name="Home" component={HomeScreen}/>
   </Stack.Navigator>
 );
 
@@ -96,7 +95,7 @@ const TabNavigator = () => (
         borderTopColor: AppStyles.color.secondarybg,
         paddingBottom: 16,
         borderTopWidth: 2,
-        height: '10%'
+        height: '10%',
         backgroundColor:'black',
       },
       tabBarInactiveTintColor: AppStyles.color.text,
