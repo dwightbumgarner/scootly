c54bc155039e7732848c1302a284adca5f5cd229
Dwight Bumgarner <dwightbumgarner@gmail.com>
2022-12-02 03:02:28 -0800

    Fix profile image for Google users

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -147,7 +147,7 @@ function ProfileScreen({navigation}){
                 <TouchableOpacity onPress={()=>selectImage()}>
                 <Image
                     style={styles.profileImage}
-                    source={image ? {uri: (image?.uri)} : (AppIcon.images.defaultProfile)}
+                    source={image ? {uri: (image?.uri)} : (auth.user.photoURL? {uri: (auth.user.photoURL)} : AppIcon.images.defaultProfile)}
                 />
             </TouchableOpacity>
             </View>
